9104461e6277368d04307c76b45b2c86013c124e
ReinerLau <lk850593913@gmail.com>
2025-07-08 09:08:02 +0800

    Clear treeData in page component to enhance flexibility in project file structure representation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,127 +6,7 @@ import type { TreeDataNode } from "antd";
 /**
  * 文件夹结构数据
  */
-const treeData: TreeDataNode[] = [
-  {
-    title: "src",
-    key: "src",
-    children: [
-      {
-        title: "app",
-        key: "app",
-        children: [
-          {
-            title: "layout.tsx",
-            key: "layout.tsx",
-            isLeaf: true,
-          },
-          {
-            title: "page.tsx",
-            key: "page.tsx",
-            isLeaf: true,
-          },
-          {
-            title: "globals.css",
-            key: "globals.css",
-            isLeaf: true,
-          },
-          {
-            title: "utils",
-            key: "utils",
-            children: [
-              {
-                title: "minio.ts",
-                key: "minio.ts",
-                isLeaf: true,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: "public",
-    key: "public",
-    children: [
-      {
-        title: "file.svg",
-        key: "file.svg",
-        isLeaf: true,
-      },
-      {
-        title: "globe.svg",
-        key: "globe.svg",
-        isLeaf: true,
-      },
-      {
-        title: "next.svg",
-        key: "next.svg",
-        isLeaf: true,
-      },
-      {
-        title: "vercel.svg",
-        key: "vercel.svg",
-        isLeaf: true,
-      },
-      {
-        title: "window.svg",
-        key: "window.svg",
-        isLeaf: true,
-      },
-    ],
-  },
-  {
-    title: "package.json",
-    key: "package.json",
-    isLeaf: true,
-  },
-  {
-    title: "package-lock.json",
-    key: "package-lock.json",
-    isLeaf: true,
-  },
-  {
-    title: "pnpm-lock.yaml",
-    key: "pnpm-lock.yaml",
-    isLeaf: true,
-  },
-  {
-    title: "next.config.ts",
-    key: "next.config.ts",
-    isLeaf: true,
-  },
-  {
-    title: "tsconfig.json",
-    key: "tsconfig.json",
-    isLeaf: true,
-  },
-  {
-    title: "eslint.config.mjs",
-    key: "eslint.config.mjs",
-    isLeaf: true,
-  },
-  {
-    title: "postcss.config.mjs",
-    key: "postcss.config.mjs",
-    isLeaf: true,
-  },
-  {
-    title: "docker-compose.yaml",
-    key: "docker-compose.yaml",
-    isLeaf: true,
-  },
-  {
-    title: "schema.prisma",
-    key: "schema.prisma",
-    isLeaf: true,
-  },
-  {
-    title: "README.md",
-    key: "README.md",
-    isLeaf: true,
-  },
-];
+const treeData: TreeDataNode[] = [];
 
 export default function Home() {
   const onSelect = (
@@ -150,13 +30,7 @@ export default function Home() {
   return (
     <div style={{ padding: "24px" }}>
       <Card>
-        <Tree
-          defaultExpandedKeys={["src", "app", "public"]}
-          defaultSelectedKeys={["page.tsx"]}
-          treeData={treeData}
-          onSelect={onSelect}
-          onExpand={onExpand}
-        />
+        <Tree treeData={treeData} onSelect={onSelect} onExpand={onExpand} />
       </Card>
     </div>
   );
