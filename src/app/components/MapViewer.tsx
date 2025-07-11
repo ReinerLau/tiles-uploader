e8640a558d2acd948f2c8e07c4248ca21e69185c
ReinerLau <lk850593913@gmail.com>
2025-07-11 15:34:01 +0800

    Enhance MapViewer component with marker and vector layer support
    Add a marker at the map center and introduce a vector layer to manage the marker in the MapViewer component. This update improves the map's interactivity and visual representation, enhancing the overall user experience.

--- src/app/components/MapViewer.tsx
+++ src/app/components/MapViewer.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { Map, TileLayer } from "maptalks";
+import { Map, TileLayer, Marker, VectorLayer } from "maptalks";
+
+const center = [116.39105276720194, 39.91251388643247];
 
 const MapViewer = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
@@ -12,16 +14,29 @@ const MapViewer = () => {
 
     // 创建地图实例
     const map = new Map(mapContainer.current, {
-      center: [116.39105276720194, 39.91251388643247],
+      center,
       zoom: 18,
       minZoom: 1,
       maxZoom: 19,
+      dragPitch: false,
+      dragRotate: false,
       baseLayer: new TileLayer("base", {
         urlTemplate: "/tile/{z}/{x}/{y}",
         tileSystem: [1, 1, -20037508.34, -20037508.34],
         debug: true,
       }),
     });
+    // 在地图中心创建 Marker
+    const centerMarker = new Marker(center);
+
+    // 创建矢量图层用于存放 Marker
+    const vectorLayer = new VectorLayer("center");
+
+    // 将 Marker 添加到矢量图层
+    vectorLayer.addGeometry(centerMarker);
+
+    // 将矢量图层添加到地图
+    map.addLayer(vectorLayer);
 
     mapInstance.current = map;
 
